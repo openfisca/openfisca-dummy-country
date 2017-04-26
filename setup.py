4467272bd578436dde728a855a906379ad9dea68
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-04-26 11:07:07 +0200

    Upgrade version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-Dummy-Country',
-    version = '0.1.4',
+    version = '0.1.5',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
